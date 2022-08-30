687db33ddfeb4d8491dcd3348c9d2f663b76ca58
Lovefields <hho1993@gmail.com>
2022-08-30 09:34:47 +0900

    fix[bug]: arrangement child logic

--- code.js
+++ code.js
@@ -2,12 +2,7 @@
 
 let canUse = false;
 let active = false;
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-// This shows the HTML page in "ui.html".
+
 figma.showUI(__uiFiles__.notice, { width: 300, height: 260 });
 
 figma.on("selectionchange", () => {
@@ -20,7 +15,7 @@ figma.on("selectionchange", () => {
         if (node.type == "FRAME") {
             if (node.children.length == 1) {
                 canUse = true;
-                figma.showUI(__uiFiles__.main, { width: 300, height: 50 });
+                figma.showUI(__uiFiles__.main, { width: 300, height: 110 });
             } else {
                 canUse = false;
                 figma.showUI(__uiFiles__.notice, { width: 300, height: 260 });
@@ -60,31 +55,31 @@ setInterval(() => {
                             node.insertChild(idx, clone);
                         }
 
-                        node.children[idx].x = (childX + childWidth) * i + childX;
-                        node.children[idx].y = (childY + childHeight) * j + childY;
+                        node.children[idx].setPluginData("i", String(i));
+                        node.children[idx].setPluginData("j", String(j));
                     }
                 }
             }
         }
 
-        node.findChildren((child) => {
-            if (child.x + child.width > nodeWidth || child.y + child.height > nodeHeight) {
-                child.remove();
+        node.children.forEach((child, idx) => {
+            if (idx > 0) {
+                let i = parseInt(child.getPluginData("i"));
+                let j = parseInt(child.getPluginData("j"));
+
+                child.x = (childX + childWidth) * i + childX;
+                child.y = (childY + childHeight) * j + childY;
+
+                if (child.x + child.width > nodeWidth || child.y + child.height > nodeHeight) {
+                    child.remove();
+                }
             }
         });
     }
 }, 500);
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
 figma.ui.onmessage = (msg) => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
     if (msg.type === "active") {
         active = msg.active;
     }
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    // figma.closePlugin();
 };
