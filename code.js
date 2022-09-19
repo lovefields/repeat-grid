ec34f5e5e231b4c1dc488cef348890483e727c4f
Lovefields <hho1993@gmail.com>
2022-09-19 09:42:53 +0900

    feat[gap]: add value setting

--- code.js
+++ code.js
@@ -2,8 +2,11 @@
 
 let canUse = false;
 let active = false;
-let prexNumber = 0;
-let preyNumber = 0;
+let preWidth = 0;
+let preheight = 0;
+let rowGap = 20;
+let columnGap = 20;
+let logicCount = 0;
 
 figma.showUI(__uiFiles__.notice, { width: 300, height: 260 });
 
@@ -17,7 +20,7 @@ figma.on("selectionchange", () => {
         if (node.type == "FRAME") {
             if (node.children.length == 1) {
                 canUse = true;
-                figma.showUI(__uiFiles__.main, { width: 300, height: 110 });
+                figma.showUI(__uiFiles__.main, { width: 300, height: 140 });
             } else {
                 canUse = false;
                 figma.showUI(__uiFiles__.notice, { width: 300, height: 260 });
@@ -40,54 +43,61 @@ setInterval(() => {
         let nodeHeight = node.height;
         let childWidth = node.children[0].width;
         let childHeight = node.children[0].height;
-        let childX = node.children[0].x;
-        let childY = node.children[0].y;
-        let xNumber = Math.floor(nodeWidth / (childWidth + childX));
-        let yNumber = Math.floor(nodeHeight / (childHeight + childY));
+        let xNumber = Math.floor(nodeWidth / (childWidth + columnGap));
+        let yNumber = Math.floor(nodeHeight / (childHeight + rowGap));
 
-        if (xNumber !== prexNumber || yNumber !== preyNumber) {
+        if (nodeWidth !== preWidth || nodeHeight !== preheight) {
+            logicCount = 0;
+        }
+
+        if (logicCount < 3) {
             let idx = 0;
+            logicCount += 1;
 
-            if (xNumber > 1 || yNumber > 1) {
-                for (let i = 0; i < xNumber; i += 1) {
-                    for (let j = 0; j < yNumber; j += 1) {
-                        if (!node.children[idx]) {
-                            let clone = childNode.clone();
-                            node.insertChild(idx, clone);
-                        }
-
-                        if (idx !== 0) {
-                            node.children[idx].setPluginData("i", String(i));
-                            node.children[idx].setPluginData("j", String(j));
-                        }
-
-                        idx += 1;
+            for (let i = 0; i < xNumber; i += 1) {
+                for (let j = 0; j < yNumber; j += 1) {
+                    if (!node.children[idx]) {
+                        let clone = childNode.clone();
+                        node.insertChild(idx, clone);
                     }
+
+                    node.children[idx].setPluginData("i", String(i));
+                    node.children[idx].setPluginData("j", String(j));
+
+                    idx += 1;
                 }
             }
 
             node.children.forEach((child, k) => {
-                if (k > 0) {
-                    let i = parseInt(child.getPluginData("i"));
-                    let j = parseInt(child.getPluginData("j"));
+                let i = parseInt(child.getPluginData("i"));
+                let j = parseInt(child.getPluginData("j"));
 
-                    child.x = (childX + childWidth) * i + childX;
-                    child.y = (childY + childHeight) * j + childY;
+                child.x = (columnGap + childWidth) * i + columnGap;
+                child.y = (rowGap + childHeight) * j + rowGap;
 
-                    if (child.x + child.width > nodeWidth || child.y + child.height > nodeHeight || k > idx - 1) {
+                if (child.x + child.width > nodeWidth || child.y + child.height > nodeHeight || k > idx - 1) {
+                    if (k > 0) {
                         child.remove();
                     }
                 }
             });
 
-            prexNumber = xNumber;
-            preyNumber = yNumber;
+            preWidth = nodeWidth;
+            preheight = nodeHeight;
         }
     }
 }, 500);
 
 figma.ui.onmessage = (msg) => {
-    if (msg.type === "active") {
-        active = msg.active;
+    switch (msg.type) {
+        case "active":
+            active = msg.active;
+            break;
+        case "setRowGap":
+            rowGap = msg.rowGap;
+            break;
+        case "setColumnGap":
+            columnGap = msg.columnGap;
+            break;
     }
 };
